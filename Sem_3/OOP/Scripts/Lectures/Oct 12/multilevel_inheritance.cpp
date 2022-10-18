5fa58bb6d02d962056f40a5ba07b4c0fa7e95052
Praneesh Sharma <pegasus30042004@gmail.com>
2022-10-18 23:31:48 +0530

    added oop lab5 questions

---
+++ Sem_3/OOP/Scripts/Lectures/Oct 12/multilevel_inheritance.cpp
@@ -0,0 +1,39 @@
+//WAP to demonstrate the order of call of constructors and destructors in case of multi-level inheritance.
+#include <iostream>
+using namespace std;
+
+class A{
+    public:
+        A(){
+            cout << "constructor A called" << endl;
+        }
+        ~A(){
+            cout << "destructor A called" << endl;
+        }
+};
+
+class B: public A{
+    public:
+        B(){
+            cout << "constructor B called" << endl;
+        }
+        ~B(){
+            cout << "destructor B called" << endl;
+        }
+};
+
+class C: public B{
+    public:
+        C(){
+            cout << "constructor C called" << endl;
+        }
+        ~C(){
+            cout << "destructor C called" << endl;
+        }
+};
+
+int main(){
+    C obj;
+
+    return 0;
+}
---
+++ Sem_3/OOP/Scripts/Lectures/Oct 12/multiple_inheritance.cpp
@@ -0,0 +1,40 @@
+//WAP to demonstrate the order of call of constructors and destructors in case of multiple inheritance.
+#include <iostream>
+using namespace std;
+
+class A{
+    public:
+        A(){
+            cout << "constructor A called" << endl;
+        }
+        ~A(){
+            cout << "destructor A called" << endl;
+        }
+};
+
+class B: public A{
+    public:
+        B(){
+            cout << "constructor B called" << endl;
+        }
+        ~B(){
+            cout << "destructor B called" << endl;
+        }
+};
+
+class C: public A{
+    public:
+        C(){
+            cout << "constructor C called" << endl;
+        }
+        ~C(){
+            cout << "destructor C called" << endl;
+        }
+};
+
+int main(){
+    B obj1;
+    C obj2;
+
+    return 0;
+}
