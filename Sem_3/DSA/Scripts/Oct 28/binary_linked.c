e2149bd49f78277472fd47381d32ef997ce5190c
Praneesh Sharma <pegasus30042004@gmail.com>
2022-10-28 12:52:00 +0530

    added dsa lab programs

--- Sem_3/DSA/Scripts/Oct 28/binary_linked.c
+++ Sem_3/DSA/Scripts/Oct 28/binary_linked.c
@@ -32,8 +32,41 @@ void buildTree(struct Tree *ptr){
     }
 }
 
+void printInorder(struct Tree *ptr){
+    if (ptr == NULL)
+        return;
+    printInorder(ptr->lc);
+    printf("%d ", ptr->data);
+    printInorder(ptr->rc);
+}
+
+void printPreorder(struct Tree *ptr){
+    if (ptr == NULL)
+        return;
+    printf("%d ", ptr->data);
+    printPreorder(ptr->lc);
+    printPreorder(ptr->rc);
+}
+
+void printPostorder(struct Tree *ptr){
+    if (ptr == NULL)
+        return;
+    printPostorder(ptr->lc);
+    printPostorder(ptr->rc);
+    printf("%d ", ptr->data);
+}
+
 void main(){
     struct Tree *root;
     root = (struct Tree *)malloc(sizeof(struct Tree));
     buildTree(root);
+
+    printf("\nInorder traversal of binary tree is: ");
+    printInorder(root);
+
+    printf("\n\nPreorder traversal of binary tree is: ");
+    printPreorder(root);
+
+    printf("\n\nPostorder traversal of binary tree is: ");
+    printPostorder(root);
 }
