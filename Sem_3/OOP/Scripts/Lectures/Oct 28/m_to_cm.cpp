b511d5a0e44f1eea6f8fabc06c8f29c083a06312
Praneesh Sharma <pegasus30042004@gmail.com>
2022-10-28 08:35:09 +0530

    added m_to_cm program

---
+++ Sem_3/OOP/Scripts/Lectures/Oct 28/m_to_cm.cpp
@@ -0,0 +1,39 @@
+//meters to centimeters conversion
+#include <iostream>
+using namespace std;
+
+class Metres{
+    int m;
+    public:
+        Metres(){}
+        Metres(int x){
+            m = x;
+        }
+        int get(){
+            return(m*100);
+        }
+};
+
+class centi{
+    int c;
+    public:
+        centi(){}
+        void showB(){
+            cout << "Centimetres = " << c << endl;
+        }
+        void operator =(Metres z){
+            c = z.get();
+        }
+};
+
+
+int main(){
+    int val;
+    cout << "Enter metres: ";
+    cin >> val;
+    Metres M(val);
+    centi C;
+    C = M;
+    C.showB();
+    return 0;
+}
