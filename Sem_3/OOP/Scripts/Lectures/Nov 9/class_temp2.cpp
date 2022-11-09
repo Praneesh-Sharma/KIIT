b1efdfb7d3ba8d3c239a5eebf4c1a6fc6d6ae577
Praneesh Sharma <pegasus30042004@gmail.com>
2022-11-09 10:00:48 +0530

    added q3 of lab 9

---
+++ Sem_3/OOP/Scripts/Lectures/Nov 9/class_temp2.cpp
@@ -0,0 +1,20 @@
+//Rewrite program ii. using class template
+#include <iostream>
+using namespace std;
+
+template<class T>
+class A{
+    public:
+        void add(T a, T b){
+            cout << a+b << endl;
+        }
+};
+int main(){
+    A<int>obj1;
+    obj1.add(2,3);
+    A<float>obj2;
+    obj2.add(2.5, 3.5);
+    A<string>obj3;
+    obj3.add("Hello" , " World");
+    return 0;
+}
